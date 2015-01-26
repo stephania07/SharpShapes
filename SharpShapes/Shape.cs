f027a82970966646c1e3c67c79e3f51591c5759b
stephania07 <stephaniaiyob@yahoo.com>
2015-01-26 10:15:42 -0600

    initial commit

--- SharpShapes/Shape.cs
+++ SharpShapes/Shape.cs
@@ -7,7 +7,7 @@ using System.Drawing;
 
 namespace SharpShapes
 {
-    public abstract class Shape
+    abstract public class Shape
     {
         
         //Eg. individual properties could be fill color...
@@ -22,28 +22,35 @@ namespace SharpShapes
         public Color BorderColor { get; set; }
 
 
-        //virtual means can overiden by subclass
+        //virtual means can be overiden by subclass
         /// <summary>
         /// The number of sides of this shape
         /// </summary>
-        public virtual int SidesCount { get; }//as length in js 
+        /// //abstract here is because it is different for every shape
+        abstract public int SidesCount { get; }//as length in js 
+
+        public Shape()
+        {
+            BorderColor = Color.Tomato;
+            FillColor = Color.Bisque;
+        }
 
          /// <summary>
          /// Calculates the area of the shape.
          /// </summary>
          /// <returns>the area of the shape</returns>
-        public abstract decimal Area();
+        abstract public decimal Area();
 
         /// <summary>
         /// Calculates the perimeter of he shape
         /// </summary>
         /// <returns>the perimeter of the shape</returns>
-        public abstract int Perimeter();
+        abstract public decimal Perimeter();
     
         /// <summary>
         /// Scales the shape in size
         /// </summary>
         /// <param name="percent">the percentage by which to scale the shape</param>
-        public abstract void scale(int percent);
+        abstract public void Scale(int percent);
     }
 }
